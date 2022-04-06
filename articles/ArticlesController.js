77eec07f7f370f58f8b758f3f4c79dcf0e33297e
Matheus Bonani <bonani.matheus@gmail.com>
2022-04-06 10:42:29 -0300

    criando rotas para adição/deleção de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,10 +1,61 @@
 const expres = require('express');
 const router = expres.Router();
+const Category = require('../categories/Category');
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get('/articles', (req, res) => {
-    
-    res.render('admin/articles/new.ejs');
+
+router.get('/admin/articles', (req, res) => {
+    Article.findAll({
+        include: [{model: Category}]  //Incluindo na busca
+    }).then(articles => {
+        res.render('admin/articles/index', {articles: articles})
+    });
 })
 
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new.ejs', {categories: categories})
+    })
+    
+})
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+})
+
+
+router.post("/articles/delete", (req, res) =>{
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles");
+            });
+
+        }else{ // não for um numero
+            res.redirect('/admin/articles');        
+        }
+
+    }else{  // underfined
+        res.redirect('/admin/articles');
+    }
+});
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('blog', 'thorn', 'murtagh', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
