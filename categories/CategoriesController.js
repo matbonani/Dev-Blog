8a533d845c235f3bce37a5379df643a48acee69e
Matheus Bonani <bonani.matheus@gmail.com>
2022-04-05 21:32:22 -0300

    funcionalidades de delete e update category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -18,7 +18,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect('/');
+            res.redirect('/admin/categories');
         })
 
     }else{
@@ -27,7 +27,62 @@ router.post("/categories/save", (req, res) => {
 });
 
 router.get("/admin/categories", (req, res) => {
-    res.render("admin/categories/index");
+        Category.findAll().then(categories => {
+            res.render("admin/categories/index", {categories: categories});
+        });
+        
 });
 
+router.post("/categories/delete", (req, res) =>{
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+
+        }else{ // não for um numero
+            res.redirect('/admin/categories');        
+        }
+
+    }else{  // underfined
+        res.redirect('/admin/categories');
+    }
+});
+
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit", {category: category});
+
+        }else{
+            res.redirect('/admin/categories');
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+})
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    Category.update({title: title, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
+});
 module.exports = router;
